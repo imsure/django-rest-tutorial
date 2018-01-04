6331187e743d3cacef33db6a53da7aa1e68ff596
Shuo Yang <imsure95@gmail.com>
2018-01-04 15:25:44 -0700

    make snippet plural

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from snippets import views
 
 urlpatterns = [
-    url(r'^snippet/$', views.snippet_list),
-    url(r'^snippet/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
