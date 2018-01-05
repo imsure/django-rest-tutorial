05a8ffcb34d60a72faff88e80be8b4f9e387694b
Shuo Yang <imsure95@gmail.com>
2018-01-05 15:47:27 -0700

    tutorial part 7: coreapi schema

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -10,4 +13,5 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
 ]
